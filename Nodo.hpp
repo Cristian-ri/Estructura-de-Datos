91c51c6ff1724497894e2ff3688c79a80b2331f4
CristianRivera919 <renan.m1015@gmail.com>
2024-02-24 12:22:46 -0600

    Creación  clase Nodo.
    Se creo la clase nodo, más adelante se mejorara y se usaran los miembros de datos de manera correcta o parte privada.

---
+++ Nodo.hpp
@@ -0,0 +1,12 @@
+#ifndef METODOSNUMERICOS
+#define METODOSNUMERICOS
+
+class Nodo{
+	public:
+		int key;
+		Nodo *next;
+		Nodo(); //Constructor por defecto
+		Nodo(int); //Sobrecargando constructor	
+};
+
+#endif
