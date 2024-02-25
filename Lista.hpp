b5fa79e14ff8384535beaa0c8afe379d3fa3bc14
CristianRivera919 <renan.m1015@gmail.com>
2024-02-25 17:53:52 -0600

    Nuevo comienzo.
    Se trabajara por separado cada clase.

--- Lista.hpp
+++ Lista.hpp
@@ -8,17 +8,17 @@ class Lista{
 		Lista();
 		//Funciones miembros
 		//Funcion ingresa un dato por enfrente de la lista
-		virtual void pushfront(int);
+		void pushfront(int);
 		//Muestra la lista
-		virtual void imprimir();
+		void imprimir();
 		//Ingresar elemento en la parte final de la lista
-		virtual void pushback(int);
+		void pushback(int);
 		//Elimina datos al inicio de la lista
-		virtual int popfront();
+		int popfront();
 		//Elimina datos al fian de la lista
-		virtual int popback();
+		int popback();
 		//Verifica si la lista esta vacia
-		virtual bool empty();
+		bool empty();
 		
 	
 	private:
